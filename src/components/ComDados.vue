7128a5fae84478638173af2ed40c828bbf124e3a
eriick-monteiro <erick.m.anjos@gmail.com>
2025-09-08 14:56:32 -0300

    :sparkles: feat: header ComDados

---
+++ src/components/ComDados.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Componente com dados</h1>
+        <p>Nome: caneta - #123</p>
+    </div>
+</template>
+
+
+<style scoped>
+    div {
+        text-align: center;
+        border: 2px solid rgb(10, 69, 4);
+        background-color: #050d04;
+        border-radius: 15px;
+        margin: 30px;
+        padding: 10px;
+    }
+</style>
+
+
+<script>
+    
+</script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
-        <h1>Hello, World!</h1>
-        <p>Meu primeiro componente Vue</p>
+        <h1>Primeiro Componente</h1>
     </div>
 </template>
+
+<style scoped>
+    div {
+        background-color: green;
+        padding: 20px;
+        margin: 30px;
+        border-radius: 5px;
+        text-align: center;
+    }
+
+    div:hover {
+        background-color: #13b939;
+    }
+
+    h1 {
+        margin-bottom: 10px;
+        color: white;
+    }
+</style>
