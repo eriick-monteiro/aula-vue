7d1abfc8716d306dd47c73e47ee1e2bfe4251457
eriick-monteiro <erick.m.anjos@gmail.com>
2025-09-10 18:10:39 -0300

    :sparkles: feat: Eventos Emit e Listener

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,21 @@
   import ComDadosReativosPrimitivos from './components/ComDadosReativosPrimitivos.vue';
   import ComDadosReativosNaoPrimitivos from './components/ComDadosReativosNaoPrimitivos.vue';
   import DataBinding from './components/DataBinding.vue';
+  import ComProps from './components/ComProps.vue';
+  import EscutaEvento from './components/EscutaEvento.vue';
+
+  const borracha = "borracha"
+  const precoBorracha = 3.50
 </script>
 
 <template>
   <header>Vue</header>
+  <EscutaEvento />
+  <ComProps item="Borracha" :preco="2.50"/>
+  <ComProps :item="borracha" :preco="precoBorracha"/>
   <DataBinding/>
   <ComDadosReativosNaoPrimitivos/>
   <ComDadosReativosPrimitivos/>
   <ComDados/>
   <PrimeiroComponente/>
 </template>
-
-
---
+++ src/components/EscutaEvento.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="componente">
+        <h1>Componente que Escuta Evento</h1>
+        <p>Total: R$ {{ valorTotal }}</p>
+        <div class="lista">
+            <EmiteEvento item="Item1" :preco="1.99" @foiAdicionado="atualizarTotal"/>
+            <EmiteEvento item="Item2" :preco="5.00" @foiAdicionado="atualizarTotal"/>
+            <EmiteEvento item="Item3" :preco="10.99" @foiAdicionado="atualizarTotal"/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import EmiteEvento from './EmiteEvento.vue';
+    import { ref } from 'vue';
+    const valorTotal = ref(0);
+
+    function atualizarTotal(preco) {
+        const valor = valorTotal.value + preco;
+        
+        valorTotal.value = +valor.toFixed(2);
+    }
+</script>
+
+<style scoped>
+    .lista {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
