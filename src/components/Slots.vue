96b420d96ac95545d4937f00fd8b505003e45f32
eriick-monteiro <erick.m.anjos@gmail.com>
2025-09-12 11:31:28 -0300

    :sparkles: feat: Propriedade Slot

---
+++ src/components/Slots.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="componente">
+        <h1>Componente com Slots</h1>
+        <button>
+            <slot name="icone"/>
+            <slot name="texto">
+                Botão <!-- Esse seria o valor padrão se nada for passado -->
+            </slot>
+        </button>
+    </div>
+</template>
